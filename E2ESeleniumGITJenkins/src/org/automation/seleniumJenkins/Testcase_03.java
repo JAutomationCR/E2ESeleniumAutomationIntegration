21d36c345504d8cf333db3a193c543753888e78d
MOHANGNANARAJ <MOHANGNANARAJ@JesusChrist>
2019-03-30 22:27:40 -0500

    FBLoginAutomation_E2EIntegration

---
+++ E2ESeleniumGITJenkins/src/org/automation/seleniumJenkins/Testcase_03.java
@@ -0,0 +1,21 @@
+package org.automation.seleniumJenkins;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Testcase_03 {
+	@Test
+	public void FBLogin() {
+		
+		System.setProperty("webdriver.gecko.driver", "./DriverFiles/geckodriver.exe");
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.get("https://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("Username1");
+		driver.findElement(By.id("pass")).sendKeys("password1");
+		driver.quit();
+	
+	}
+
+}
